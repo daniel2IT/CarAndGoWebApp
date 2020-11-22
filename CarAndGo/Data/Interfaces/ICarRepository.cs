e4c49bf0be2eaff6fdf9a8535cb7d08eb7bd83b9
Daniel <50612327+daniel2IT@users.noreply.github.com>
2020-11-22 21:38:11 +0200

    Interfaces

---
+++ CarAndGo/Data/Interfaces/ICarRepository.cs
@@ -0,0 +1,21 @@
+﻿using CarAndGo.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarAndGo.Data.Interfaces
+{
+    public interface ICarRepository
+    {
+        /* use to get all Cars */
+        IEnumerable<Car> Car { get; set; }
+
+        /* Setted Cars */
+        IEnumerable<Car> PreferredCars { get; set; }
+
+        /* for one */
+        Car getCarById(int carId);
+
+    }
+}
---
+++ CarAndGo/Data/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using CarAndGo.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarAndGo.Data.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        /* hold all categories */
+        IEnumerable<Category> Categories { get; }
+
+
+    }
+}
