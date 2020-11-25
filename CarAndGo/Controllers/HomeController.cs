371fd903273840f11ab3e7b7e610a7ad41189cb3
Daniel <50612327+daniel2IT@users.noreply.github.com>
2020-11-25 20:01:45 +0200

    Added_Home_Controller

---
+++ CarAndGo/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using CarAndGo.Data.Interfaces;
+using CarAndGo.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarAndGo.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private ICarRepository _carRep;
+
+        public HomeController(ICarRepository carRep)
+        {
+            _carRep = carRep;
+        }
+
+        public ViewResult Index()
+        {
+            var homeCars = new HomeViewModel
+            {
+                favCars = _carRep.PreferredCars
+            };
+            return View(homeCars); /* Objektai kurie bus atvaizduojami General Page */
+        }
+
+    }
+}
---
+++ CarAndGo/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CarAndGo.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarAndGo.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Car> favCars { get; set; }
+    }
+}
